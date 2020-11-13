b0b8f362d55d95089fd71af47ceefa41a1011808
Damie <jogungbure@gmail.com>
2020-11-13 07:59:17 +0100

    Finished account updating and image upload

--- src/forms.py
+++ src/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_wtf.file import FileField, FileAllowed
 from src.models import User
 
 #Signup form class
@@ -28,3 +30,23 @@ class SignInForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()])
     rem_me = BooleanField('Remember me')
     submit = SubmitField("Sign In")
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
